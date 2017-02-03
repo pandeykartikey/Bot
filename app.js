b4eb5971a92b8d6de9b40c82f56ae6ff839d6b16
Kartikey Pandey <k.p.2511998@gmail.com>
2017-02-03 11:49:29 +0530

    Now you can add or remove task

--- app.js
+++ app.js
@@ -2,7 +2,36 @@ var builder = require('botbuilder');
 
 var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
-bot.dialog('/', [
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.matches(/^Add task/i, [
+    function (session) {
+        session.beginDialog('/task');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your task has been added %s', session.userData.task);
+    }
+]);
+
+intents.matches(/^Show task/i, [
+
+    function (session) {
+        session.send('Ok... Changed your task is  %s', session.userData.task);
+    }
+]);
+
+intents.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
             session.beginDialog('/profile');
@@ -12,6 +41,16 @@ bot.dialog('/', [
     },
     function (session, results) {
         session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/task', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your task?');
+    },
+    function (session, results) {
+        session.userData.task = results.response;
+        session.endDialog();
     }
 ]);
 
