86c61ed9db71b9396704a189d2eee3b0fff9a933
Kartikey Pandey <k.p.2511998@gmail.com>
2017-02-03 12:32:40 +0530

    Added show task

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
 var intents = new builder.IntentDialog();
 bot.dialog('/', intents);
-
+var task=[];
+var i=-1;
 
 intents.matches(/^change name/i, [
     function (session) {
@@ -20,16 +21,43 @@ intents.matches(/^Add task/i, [
         session.beginDialog('/task');
     },
     function (session, results) {
-        session.send('Ok... Changed your task has been added %s', session.userData.task);
+        session.send('Ok... Your task has been added %s', task[i]);
     }
 ]);
 
 intents.matches(/^Show task/i, [
 
     function (session) {
-        session.send('Ok... Changed your task is  %s', session.userData.task);
+    	session.send('Task yet to be completed are -->')
+    	for (var j = i; j >= 0; j--) {
+    	session.send('%s',task[j]);
+    }}
+]);
+
+intents.matches(/^remove task/i, [
+    function (session) {
+        session.beginDialog('/task1');
     }
 ]);
+
+bot.dialog('/task1', [
+    function (session) {
+        builder.Prompts.text(session, 'Which task have you completed?');
+    },
+    function (session, results) {
+        var rem =results.response;
+        var index=task.indexOf(rem);
+        if(index==-1){
+         session.send("Task does not exist");}
+         else {
+         	task.splice(index, 1);
+         	i--;
+         	session.send('Ok... Changed your task has been removed');
+         }
+        session.endDialog();
+    }
+]);
+
 
 intents.onDefault([
     function (session, args, next) {
@@ -40,16 +68,17 @@ intents.onDefault([
         }
     },
     function (session, results) {
-        session.send('Hello %s!', session.userData.name);
+        session.send('What do you want to do? %s!', session.userData.name);
     }
 ]);
 
 bot.dialog('/task', [
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your task?');
+        builder.Prompts.text(session, 'Hi! What do you want to add?');
     },
     function (session, results) {
-        session.userData.task = results.response;
+        task.push(results.response);
+        i++;
         session.endDialog();
     }
 ]);
