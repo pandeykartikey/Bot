3e372fafab07c69956d29717925dd940ea46f102
Kartikey Pandey <k.p.2511998@gmail.com>
2017-02-07 23:04:20 +0530

    Added

--- app.js
+++ app.js
@@ -71,11 +71,12 @@ bot.dialog('/show', [
             session.send('No task remaining');
         }}
         else{
-var  da=results.response;
+var  da=String(results.response);
         var reg=/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/ ;
         if(da.match(reg)==null){
-        builder.Prompts.text(session, 'Please send the date in the format dd/mm/yyyy or dd-mm-yyyy')
-        session.beginDialog('/date');
+        session.send( 'Please send the date in the format dd/mm/yyyy or dd-mm-yyyy');
+        session.endDialog();
+        session.beginDialog('/show');
         }
         else{
         var today= new Date();
@@ -84,18 +85,18 @@ var  da=results.response;
         var day=da.substr(0,2);
         var y2k  = new Date(yr, month-1, day); 
         temp =Date.daysBetween(today,y2k);
-        }
+        
         var diff=temp;
             session.send('Task yet to be completed on %s are -->',da);
          for (var j = i; j >= 0; j--) {
                 if(diff==time[j].t){
                 session.send('%s', time[j].head);}
-            }
-        }
+                }
+        
         session.send('What do you want to do now? %s!', name);
         session.endDialog();
 
-    }
+    }}}
 ]);
 
 
